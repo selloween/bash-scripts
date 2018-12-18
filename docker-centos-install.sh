679dd3579b3c465af1daa4c1cb9b6a05c4a68e2a
Selwyn Rogers <selwyn.rogers@eodc.eu>
2018-12-18 20:24:52 +0100

    initial commit

---
+++ docker-centos-install.sh
@@ -0,0 +1,22 @@
+echo "###### Installing dependencies ######"
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+echo "###### Adding Docker Repo ######"
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+echo "###### Installing Docker CE ######"
+sudo yum install docker-ce
+
+echo "Starting Docker Daemon"
+sudo systemctl start docker
+
+echo "Adding Docker Group"
+sudo groupadd docker
+
+echo "Adding ${USER} to Docker Group"
+sudo usermod -aG docker $USER
+
